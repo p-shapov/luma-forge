d4461cd5b903a4ec7017ab84758dd78f2bd354bc
p-shapov <pav.shapov@gmail.com>
2026-06-06 19:38:15 +0500

    refactor(placement): remove RemotePlacementCapabilities and update RemotePlacementPlan to use keep_alive_limits

--- src-tauri/src/domain/placement.rs
+++ src-tauri/src/domain/placement.rs
@@ -3,11 +3,6 @@ use serde::{Deserialize, Serialize};
 use super::provider::GpuCloudProviderId;
 
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
-pub struct RemotePlacementCapabilities {
-    pub remote_endpoint_keep_alive: RemoteEndpointKeepAliveCapability,
-}
-
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct RemoteGpuPlacementOption {
     pub id: String,
     pub name: String,
@@ -29,23 +24,11 @@ pub struct RemotePlacementOptions {
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
-#[serde(tag = "supported", rename_all = "snake_case")]
-pub enum Capability<T> {
-    #[serde(rename = "true")]
-    Supported(T),
-
-    #[serde(rename = "false")]
-    Unsupported,
-}
-
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct RemoteEndpointKeepAliveLimits {
     pub default_seconds: u32,
     pub min_seconds: u32,
     pub max_seconds: u32,
 }
-
-pub type RemoteEndpointKeepAliveCapability = Capability<RemoteEndpointKeepAliveLimits>;
 
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct RemotePlacementPlan {
@@ -53,5 +36,5 @@ pub struct RemotePlacementPlan {
     pub datacenter_id: String,
     pub gpu_id: String,
     pub volume_size_bytes: u64,
-    pub capabilities: RemotePlacementCapabilities,
+    pub keep_alive_limits: Option<RemoteEndpointKeepAliveLimits>,
 }
